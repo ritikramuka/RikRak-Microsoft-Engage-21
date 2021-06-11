5990034741cafcde4bc78fc6ddd4d1937db6c9c8
ritikramuka <ritik.ramuka@gmail.com>
2021-06-11 19:34:28 +0530

    Basic React-Route implemented

--- frontend/rikrak/src/App.js
+++ frontend/rikrak/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LoginScreen from './screens/LoginScreen';
+import VideoCallScreen from './screens/VideoCallScreen';
+import HomeScreen from './screens/HomeScreen';
+import ResetPasswordScreen from './screens/ResetPasswordScreen';
+import ForgetPasswordScreen from './screens/ForgetPasswordScreen';
+import CreatVideoCallScreen from './screens/CreatVideoCallScreen';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <LoginScreen></LoginScreen>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path='/' component={HomeScreen} exact />
+        <Route path='/login' component={LoginScreen} />
+        <Route path='/reset' component={ResetPasswordScreen} />
+        <Route path='/forget' component={ForgetPasswordScreen} />
+        <Route path='/create' component={CreatVideoCallScreen} />
+        <Route path='/Call' component={VideoCallScreen} />
+      </div>
+    </Router>
   );
 }
 
