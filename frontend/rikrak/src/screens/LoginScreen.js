db9ac57282be23f302499bad834635d6ef23598f
ritikramuka <ritik.ramuka@gmail.com>
2021-06-11 20:44:51 +0530

    formating changed

--- frontend/rikrak/src/screens/LoginScreen.js
+++ frontend/rikrak/src/screens/LoginScreen.js
@@ -15,19 +15,19 @@ function LoginScreen() {
                         <Form>
                             <div className='Form-Heading'>Welcome!</div>
                             <div className='Form-SubHeading'>Login to your Account</div>
-                            <Form.Group controlId="formBasicEmail">
+                            <Form.Group controlId='formBasicEmail'>
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" className='LoginScreen__Form-input' />
-                                <Form.Text className="text-muted">
+                                <Form.Control type='email' placeholder='Enter email' className='LoginScreen__Form-input' />
+                                <Form.Text className='text-muted'>
                                     We'll never share your email with anyone else.
     </Form.Text>
                             </Form.Group>
 
-                            <Form.Group controlId="formBasicPassword">
+                            <Form.Group controlId='formBasicPassword'>
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" className='LoginScreen__Form-input' />
+                                <Form.Control type='password' placeholder='Password' className='LoginScreen__Form-input' />
                             </Form.Group>
-                            <Button variant="primary" type="submit">
+                            <Button variant='primary' type='submit' size='lg'>
                                 Login
   </Button>
                         </Form>
