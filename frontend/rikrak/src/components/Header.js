99efada46f25e063b4ab420d03131d4fc43b47a4
ritikramuka <ritik.ramuka@gmail.com>
2021-06-11 20:13:30 +0530

    Added nav items to Header as per current location

--- frontend/rikrak/src/components/Header.js
+++ frontend/rikrak/src/components/Header.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Header() {
+
+    // getting current location and setting nav elements w.r.t. to it
+    const [nav, setNav] = useState('Home');
+    useEffect(() => {
+        const url = window.location.href;
+        const currentLocation = url.substr(url.lastIndexOf('/'));
+
+        if (currentLocation === '/') setNav('Home');
+        else if (currentLocation === '/login') setNav('Login');
+    }, [nav]);
+
     return (
         <div>
             <Navbar bg='primary' variant='dark'>
                 <Navbar.Brand href='/'>RikRak</Navbar.Brand>
                 <Nav className='ml-auto'>
-                    <Link to='/login'>
-                        <Button variant='dark'>Login</Button>
-                    </Link>
+                    {nav !== 'Home' ? (
+                        <Link to='/'>
+                            <Button variant='dark' className='mr-2' onClick={() => setNav('Home')}>Home</Button>
+                        </Link>
+                    ) : null}
+                    {nav !== 'Login' ? (
+                        <Link to='/login'>
+                            <Button variant='dark' className='mr-2' onClick={() => setNav('Login')}>LogIn</Button>
+                        </Link>
+                    ) : null}
+                    {nav !== 'Signin' ? (
+                        <Link to='/signin'>
+                            <Button variant='dark' className='mr-2' onClick={() => setNav('Signin')}>SignIn</Button>
+                        </Link>
+                    ) : null}
                 </Nav>
             </Navbar>
         </div>
